2bd17debec84057f9ef54be2cbd590c763fbe1f3
ajaikmr <96674616+ajaikmr@users.noreply.github.com>
2022-03-29 01:01:51 +0530

    Delete videosdjango directory

--- videosdjango/web/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from web.models import video #add
-
-# Create your views here.
-def home(request):
-	content = video.objects.all()
-	return render(request,'index.html',{'content':content,})
